6c493f93513d59e037a1f85e28cc8831a7a5f1ed
azuldev <91290695+redazul@users.noreply.github.com>
2022-11-02 10:02:04 -0400

    update

---
+++ init.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt upgrade
+sudo apt-get install ubuntu-desktop
+sudo apt install xrdp
+sudo systemctl status xrdp
+sudo adduser xrdp ssl-cert
+sudo adduser jc
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install ffmpeg obs-studio
