b6f25b8b676d88372865b153b5e7d3c4003a45d5
azuldev <91290695+redazul@users.noreply.github.com>
2022-11-06 17:16:03 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,5 @@ sudo systemctl status xrdp
 sudo adduser xrdp ssl-cert
 sudo adduser jc
 sudo add-apt-repository ppa:obsproject/obs-studio
-sudo apt update
 sudo apt install ffmpeg obs-studio
 sudo ufw allow from any to any port 3389 proto tcp
