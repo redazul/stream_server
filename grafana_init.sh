7a677a8ca1cc4d57bc42ed46f6180f508bf8b2ec
azuldev <91290695+redazul@users.noreply.github.com>
2022-11-07 23:32:12 -0500

    Update grafana_init.sh

--- grafana_init.sh
+++ grafana_init.sh
@@ -1,10 +1,11 @@
-sudo apt-get update
+sudo apt update
+sudo apt upgrade
 sudo apt-get install -y apt-transport-https
 sudo apt-get install -y software-properties-common wget
 sudo wget -q -O /usr/share/keyrings/grafana.key https://packages.grafana.com/gpg.key
 echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt-get update
-sudo apt-get install grafana
+sudo apt update
+sudo aptinstall grafana
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
 sudo systemctl status grafana-server
